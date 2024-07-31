c936e850ed6753461b1cb03e506f1da9dd147df4
Itakello <maxste000@gmail.com>
2024-08-01 01:23:46 +0200

    Fixed to latest openai library version

--- classes/chatGPT.py
+++ classes/chatGPT.py
@@ -1,18 +1,22 @@
-from dataclasses import dataclass
-import openai
-from .recordings import Language
-from .chunk import Chunk
-import os
 import json
+import os
 import time
+from dataclasses import dataclass, field
+
+from openai import OpenAI
+
+from .chunk import Chunk
+from .recordings import Language
+
 
 @dataclass
-class ChatGPTUtils():
-    
-    def __post_init__(self):
-        openai.api_key = os.environ.get("OPENAI_API_KEY")
-        
-    def call_openai_api(self, content:str, prompt:str, retries:int = 0) -> dict:
+class ChatGPTUtils:
+    model: str
+    client: OpenAI = field(
+        default=OpenAI(api_key=os.environ.get("OPENAI_API_KEY")), init=False
+    )
+
+    def call_openai_api(self, content: str, prompt: str, retries: int = 0) -> dict:
         MAX_RETRIES = 5
         if retries > MAX_RETRIES:
             raise Exception("Reached maximum number of retries.")
@@ -23,7 +27,9 @@ class ChatGPTUtils():
         except json.decoder.JSONDecodeError as je:
             print(je)
             time.sleep(10)
-            je_content, je_propmpt = self._create_content_and_prompt_json_error(response, je)
+            je_content, je_propmpt = self._create_content_and_prompt_json_error(
+                response, je
+            )
             response = self._invoke_prompt(je_content, je_propmpt)
             parsed_res = json.loads(response)
         except Exception as e:
@@ -31,36 +37,45 @@ class ChatGPTUtils():
             time.sleep(10)
             parsed_res = self.call_openai_api(content, prompt, retries)
         return parsed_res
-    
-    def get_additional_info(self, chunk_str:str, language:Language) -> Chunk:
+
+    def get_additional_info(self, chunk_str: str, language: Language) -> Chunk:
         content, prompt = self._create_content_and_prompt(chunk_str, language)
         parsed_res = self.call_openai_api(content, prompt)
         chunk = self._create_chunk(chunk_str, parsed_res)
         return chunk
-    
-    def _create_chunk(self, transcription: str, parsed_res:dict) -> Chunk:
-        return Chunk(parsed_res['title'], transcription, parsed_res['summary'], parsed_res['main_points'], parsed_res['follow_up'])
-    
-    def _invoke_prompt(self, content:str, prompt:str, max_retries=3, delay=10) -> str:
+
+    def _create_chunk(self, transcription: str, parsed_res: dict) -> Chunk:
+        return Chunk(
+            parsed_res["title"],
+            transcription,
+            parsed_res["summary"],
+            parsed_res["main_points"],
+            parsed_res["follow_up"],
+        )
+
+    def _invoke_prompt(self, content: str, prompt: str, max_retries=3, delay=10) -> str:
         for retry in range(max_retries):
             try:
-                response = openai.ChatCompletion.create(
-                    model="gpt-3.5-turbo",
+                response = self.client.chat.completions.create(
+                    model=self.model,
                     messages=[
-                        {"role": "system", "content":content},
-                        {"role": "user", "content": prompt}
+                        {"role": "system", "content": content},
+                        {"role": "user", "content": prompt},
                     ],
-                    temperature=0.1
-                )       
-                return str(response.choices[0]['message']['content'])
+                    temperature=0.1,
+                )
+
+                return str(response.choices[0]["message"]["content"])
             except Exception as e:
                 if retry < max_retries - 1:  # It's not the final retry
-                    time.sleep(delay) 
+                    time.sleep(delay)
                     continue
                 else:
                     raise ConnectionError
-    
-    def _create_content_and_prompt(self, transcription:str, language:Language) -> tuple[str, str]:
+
+    def _create_content_and_prompt(
+        self, transcription: str, language: Language
+    ) -> tuple[str, str]:
         if language == Language.ENGLISH:
             prompt = f'Analyze the transcript of a lesson provided below, which could contain errors especially at the start and end of the text, then provide the following:\nKey "title:" - add a title.\n\nKey "main_points" - add an array of the main points. Limit each item to 100 words, and limit the list to 10 items.\nKey "follow_up:" - add an array of follow-up questions. Limit each item to 100 words, and limit the list to 5 items.\nKey "summary" - create a really detailed in-depth summary of at least 250 words.\n\nEnsure that the final element of any array within the JSON object is not followed by a comma.\nTranscript:\n{transcription}'
             content = 'You are an assistant that only speaks JSON. Do not write normal text.\nExample formatting:\n{\n"title": "Notion Buttons",\n"main_points": [\n"item 1",\n"item 2",\n"item 3"\n],\n"follow_up": [\n"item 1",\n"item 2",\n"item 3"\n],,\n"summary": "A detailed description of buttons for Notion"}'
@@ -70,8 +85,10 @@ class ChatGPTUtils():
         else:
             raise NotImplementedError("Language not supported")
         return content, prompt
-    
-    def _create_content_and_prompt_json_error(self, json:str, error:str) -> tuple[str, str]:
-        prompt = f'You have an error in the JSON formatting. Please fix it and print it out.\nError: {error}\nJSON: {json}'
+
+    def _create_content_and_prompt_json_error(
+        self, json: str, error: str
+    ) -> tuple[str, str]:
+        prompt = f"You have an error in the JSON formatting. Please fix it and print it out.\nError: {error}\nJSON: {json}"
         content = 'You are an assistant that only speaks JSON. Do not write normal text.\nExample formatting:\n{\n"title": "Notion Buttons",\n"main_points": [\n"item 1",\n"item 2",\n"item 3"\n],\n"follow_up": [\n"item 1",\n"item 2",\n"item 3"\n],\n"summary": "A detailed description of buttons for Notion"}'
         return content, prompt
--- main.py
+++ main.py
@@ -1,28 +1,39 @@
+from pathlib import Path
 
 import whisper
 from tqdm import tqdm
 
 from classes.chatGPT import ChatGPTUtils
 from classes.notion import NotionPage
-from classes.utils import *
-from pathlib import Path
+from classes.utils import (
+    get_chunks_from_transcription,
+    get_paths,
+    get_recording,
+    move_to_folder,
+)
 
-def main():
+
+def main() -> None:
     paths = get_paths(Path("/mnt/c/Users/maxst/OneDrive/class_recordings"))
-    gpt_utils = ChatGPTUtils()
+    gpt_utils = ChatGPTUtils(model="gpt-3.5-turbo")
     model = whisper.load_model("medium")
-    
+
     for path in tqdm(paths, desc="Processing recordings"):
         recording = get_recording(path)
-        transcription = model.transcribe(str(recording.audio_path))['text']
-        chunks = get_chunks_from_transcription(str(transcription), max_tokens=2000) # Also take into account the input prompt and the output
+        transcription = model.transcribe(str(recording.audio_path))["text"]
+        chunks = get_chunks_from_transcription(
+            str(transcription), max_tokens=2000
+        )  # Also take into account the input prompt and the output
         notion_page = NotionPage(recording)
-        
+
         for idx, chunk in tqdm(enumerate(chunks), desc="Processing chunks"):
-            chunk_obj = gpt_utils.get_additional_info(chunk_str=chunk, language=recording.language)
+            chunk_obj = gpt_utils.get_additional_info(
+                chunk_str=chunk, language=recording.language
+            )
             notion_page.update_page(chunk_obj, idx + 1)
-            
-        move_to_folder(recording.audio_path, 'processed')
-    
+
+        move_to_folder(recording.audio_path, "processed")
+
+
 if __name__ == "__main__":
     main()
